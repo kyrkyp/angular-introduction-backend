85c1ef9374562143685d8d5d99207764b6c100e3
Kyriakos Kypraios <kkypraios@allweb.gr>
2023-11-01 08:43:59 +0200

    user services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  // GET endpoints
+
+  @Get()
+  async findAllUsers() {
+    return await this.userService.findAllUsers();
+  }
+
+  @Get(':id')
+  async findUserById(@Param('id') id: string) {
+    return await this.userService.findUserById(parseInt(id));
+  }
+
+  @Get('email/:email')
+  async findUserByEmail(@Param('email') email: string) {
+    return await this.userService.findUserByEmail(email);
+  }
+
+  // POST endpoints
+
+  @Post()
+  async createUser(@Body(new ValidationPipe()) user: UserDto) {
+    return await this.userService.createUser(user);
+  }
+
+  @Post('bulk')
+  async createUsers(@Body(new ValidationPipe()) users: UserDto[]) {
+    return await this.userService.createUsers(users);
+  }
+}
